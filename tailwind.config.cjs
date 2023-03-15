3895f9f989f4a5b9ab71150bb0b202fc6abbc115
kyleulman <kyle@ulman.digital>
2023-03-15 14:10:59 -0400

    update colors & icons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		colors: {
-			dark: '#0B090A',
-			primary: '#7F636E'
-		},
-		extend: {}
+		extend: {
+			colors: {
+				thunder: {
+					50: '#f9f6f8',
+					100: '#f2ecf0',
+					200: '#e3d4df',
+					300: '#caafc2',
+					400: '#ac84a1',
+					500: '#926586',
+					600: '#79506c',
+					700: '#634158',
+					800: '#53394b',
+					900: '#34252f'
+				}
+			}
+		}
 	},
 	plugins: []
 };
